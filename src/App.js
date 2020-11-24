9f2987febcc33f174a0e5f2078ccc959b9c81535
Mars <wilson.marissarose@gmail.com>
2020-11-23 22:59:01 -0500

    Created file structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
+import React, { useState, useEffect, useMemo} from "react";
+import FriendCard from "./components/FriendCard";
+import Title from "./components/Title";
+import friends from "./friends.json";
+import Table from "./components/Table";
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+function App(){
+
+  const [data, setData] = useState([]);
+
+// Using useEffect to call the json once mounted and set the data
+  useEffect(() => {
+    const data = friends;
+      setData(data);
+
+  }, []);
+
+  const columns = useMemo(
+    () => [
+      {
+        // first group - Employee
+        Header: "Employee",
+        // First group columns
+        columns: [
+          {
+            Header: "Name",
+            accessor: "name"
+   
+          },
+          {
+            Header: "Role",
+            accessor: "occupation"
+         
+          }
+        ]
+      },
+      {
+        // Second group - Details
+        Header: "Details",
+        // Second group columns
+        columns: [
+          {
+            Header: "Branch Location",
+            accessor: "location",
+          },
+          {
+            Header: "Headshot",
+            accessor: "image",
+            Cell: ({ cell: { value } }) => <FriendCard friend={value}/>
+          },
+        ]
+      }
+    ],
+    
+    []
   );
-}
+
+    return (
+      <div className="App">
+    
+      <Title>Employee List</Title>
+        <Table 
+        columns={columns} 
+        data={data} 
+        setTable={setData}/>
+        </div>
+    );
+  }
+
 
 export default App;
